7ecce2510db331ef328aac2a4ea0d8537c931e10
Simon Haag <simonhaag1@hotmail.com>
2022-06-07 11:59:14 +0200

    Added some testing but does not work on komplett website

--- SeleniumTests.cs
+++ SeleniumTests.cs
@@ -1,6 +1,7 @@
 using System;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
 using WebDriverManager;
 using OpenQA.Selenium.Support;
 using Xunit;
@@ -20,22 +21,28 @@ namespace CEROSIFRTestVerktygSelenium
 
         public SeleniumTests()
         {
+
             // Konstruktor för att
             // Klicka på acceptera kakor varje gång vi kör ett test
             // Båda click nedan funkar!
 
             driver.Navigate().GoToUrl("https://www.komplett.se/");
-            Thread.Sleep(1500);
-            
+            Thread.Sleep(1000);
             driver.FindElement(By.CssSelector(".btn-large")).Click();
-            driver.FindElement(By.XPath("//button[@class='btn-large primary']")).Click();
 
+            //driver.FindElement(By.XPath("//button[@class='btn-large primary']")).Click();
         }
 
         [Fact]
         public void Test1()
         {
-            //driver.Navigate().GoToUrl("https://www.komplett.se/");
+
+            IWebElement searchBar = driver.FindElement(By.Id("caasSearchInput"));
+            searchBar.SendKeys("Soundbar");
+            Thread.Sleep(2000);
+            searchBar.SendKeys(Keys.Enter);
+
+            //driver.FindElement(By.CssSelector(".search-input__icon")).Click();
         }
 
        /* [Fact]
