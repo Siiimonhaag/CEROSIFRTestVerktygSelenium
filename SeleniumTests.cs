337167db62a3c4ac374566ab33c405743c21a10a
Simon Haag <simonhaag1@hotmail.com>
2022-06-10 13:14:56 +0200

    Added test ChangeQuantity

--- SeleniumTests.cs
+++ SeleniumTests.cs
@@ -6,6 +6,7 @@ using WebDriverManager;
 using OpenQA.Selenium.Support;
 using Xunit;
 using System.Threading;
+using System.Collections.Generic;
 
 namespace CEROSIFRTestVerktygSelenium
 {
@@ -50,20 +51,71 @@ namespace CEROSIFRTestVerktygSelenium
         }
 
         [Fact]
-        public void ThisIsTheFirstTestAgainJustForShowcase()
+        [Trait("ID 1","Input form & button")]
+        public void ChangeQuantityInTheShoppingCart()
         {
+            
+            IWebElement logIn = driver.FindElement(By.XPath("//a[@title='Logga in / Mitt Coop']"));
+            logIn.Click();
+            driver.Manage().Window.FullScreen();
+            Thread.Sleep(1500);
+
+            IWebElement enterEmail = driver.FindElement(By.XPath("//input[@id='loginEmail']"));
+            enterEmail.SendKeys("testcoop123@hotmail.com");
+
+            IWebElement enterPassword = driver.FindElement(By.XPath("//input[@id='loginPassword']"));
+            enterPassword.SendKeys("Cerosifr123!");
+
+            IWebElement login2 = driver.FindElement(By.XPath("//button[@type='submit']"));
+            login2.Click();
+            Thread.Sleep(3000);
+
             IWebElement handlaOnline = driver.FindElement(By.LinkText("Handla online"));
             handlaOnline.Click();
+
             driver.Manage().Window.FullScreen();
             Thread.Sleep(1000);
+
             IWebElement searchBar = driver.FindElement(By.ClassName("Search-input"));
-            searchBar.SendKeys("Ketchup");
-            Thread.Sleep(2000);
-            searchBar.SendKeys(Keys.Enter);
+            searchBar.SendKeys("Kronfågel Majskyckling");
             Thread.Sleep(1500);
+
+            searchBar.SendKeys(Keys.Enter);
+            Thread.Sleep(2000);
+
+            IWebElement addProduct = driver.FindElement(By.XPath("//a[@aria-label='Bröstfilé Av Majskyckling']"));
+            addProduct.Click();
+            Thread.Sleep(2500);
+
+            IWebElement plusButton = driver.FindElement(By.XPath("//button[@aria-label='Öka antal']"));
+            plusButton.Click();
+            Thread.Sleep(2000);
+
+
+            IWebElement shoppingCart = driver.FindElement(By.XPath("//button[@aria-label='kundvagn']"));
+            shoppingCart.Click();
+            Thread.Sleep(2000);
+
+            IWebElement increaseQuantity = driver.FindElement(By.XPath("//button[@aria-label='Öka antal']"));
+            increaseQuantity.Click();
+            Thread.Sleep(1000);
+            string actual = driver.FindElement(By.XPath("//input[@type='numeric']")).GetAttribute("value").ToString();
+            string expected = "2";
+            Assert.Equal(expected,actual);
+
+            for (int i = 0; i < 2; i++)
+            {
+                IWebElement decreaseQuantity = driver.FindElement(By.XPath("//button[@aria-label='Minska antal']"));
+                decreaseQuantity.Click();
+                Thread.Sleep(500);
+
+            }
+            Thread.Sleep(2000);
+
             driver.Quit();
             driver.Dispose();
         }
+
         /*
         [Fact]
         public void Test3()
