7b04dae51b0107aee9907c8995734382da3df581
Simon Haag <simonhaag1@hotmail.com>
2022-08-15 10:21:27 +0200

    Updated more sleep for the goroturl and accept cookies

--- SeleniumTests.cs
+++ SeleniumTests.cs
@@ -27,9 +27,9 @@ namespace CEROSIFRTestVerktygSelenium
             // Båda click nedan funkar!
             testOutput = _testOutput;
             driver.Navigate().GoToUrl(url);
-            Thread.Sleep(1500);
+            Thread.Sleep(3000);
             driver.Manage().Window.FullScreen();
-            Thread.Sleep(1500);
+            Thread.Sleep(2000);
             driver.FindElement(By.XPath("//*[@id='cmpbntyestxt']")).Click();
 
         }
